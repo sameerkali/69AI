4a85d9fd353509e8088649c770d01837ff6d4969
Sameer Faridi <s.faridi007@gmail.com>
2023-03-30 18:50:59 +0530

    add better frontend with ❤️

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://six9-19s1.onrender.com', {
+    const response = await fetch('http://localhost:5000', {  //https://six9-19s1.onrender.com
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
